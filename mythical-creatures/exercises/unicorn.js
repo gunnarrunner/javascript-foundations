77728fcccbd4a4931ea306930c89f6b92a6ab9fd
Gunnar <dunkcrunk@gmail.com>
2022-01-10 18:29:42 -0700

    Got through the first set of exercises

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,20 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
     this.color = color;
+    }
+  }
+  isWhite() {
+    if (this.color !== 'white') {
+      return false;
+    }
+  }
+
+  says(phrase) {
+    return `**;* ${phrase} *;**`
   }
 }
 
