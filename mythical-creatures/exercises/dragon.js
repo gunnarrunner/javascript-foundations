ff70db47ca23be7cb78572511ffb115776c90c0b
Gunnar <dunkcrunk@gmail.com>
2024-02-21 18:20:35 -0700

    Practiced dragon js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,28 @@
 
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hunger = 0;
+    this.updateHungryStatus();
+  }
+
+  updateHungryStatus() {
+    if (this.hunger > 2) {
+      this.hungry = false;
+    } else {
+      this.hungry = true;
+    }
+  }
+
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+
+  eat() {
+    this.hunger += 1;
+    this.updateHungryStatus()
+  }
+}
+
+module.exports = Dragon;
